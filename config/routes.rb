12bdfa8d0691aeda24d5dc1d3cdbc4c2a5d41ea4
shimomurayuki <c0116138b7@edu.teu.ac.jp>
2019-02-20 12:45:43 +0900

    finish

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'blogs/new_contact'
   post 'blogs/new_contact', to: 'blogs#new_contact_create'
   post 'blogs/new_contact_confirmation'
+  get 'blogs/new_contact_finish'
   resources :blogs
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
